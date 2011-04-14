9e061f103b00d01141e371319e7d27c54a8db9bc
Samuel Cole <sam@samuelcole.name>
2011-04-14 14:43:39 -0400

    Move vim -> new_vim to avoid the vim block, also added multibyte support.

---
+++ Library/Formula/new_vim.rb
@@ -0,0 +1,13 @@
+require 'formula'
+
+class NewVim <Formula
+  url 'ftp://ftp.vim.org/pub/vim/unix/vim-7.3.tar.bz2'
+  homepage 'http://www.vim.org/'
+  md5 '5b9510a17074e2b37d8bb38ae09edbf2'
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--with-features=big", "--prefix=#{prefix}"
+    system "make install"
+  end
+end
